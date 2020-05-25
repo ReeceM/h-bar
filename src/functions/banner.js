d45f5601ddf1c1effdf8bef368cf86173fc4024d
ReeceM <reecestevenmay@gmail.com>
2020-05-25 22:09:58 +0200

    wip

---
+++ src/functions/banner.js
@@ -0,0 +1,147 @@
+import { domReady, newElement } from "../utils";
+
+/**
+ * Render the element.
+ */
+export function render() {
+    if (this.isDismissed()) return;
+    domReady().then(() => {
+
+        if (!this.postTitle) {
+            console.error('[h-bar] no post data, unable to render');
+            return;
+        }
+
+        let secondaryElement = null;
+
+        if (!this.dismissible) {
+            let secondaryLinkList = this.createSecondaryLinks()
+            secondaryElement = newElement('div', {
+                children: secondaryLinkList,
+                classes: `${this.styling.linkWrapper} ${themes[this.theme].linkWrapper}`
+            })
+        } else {
+            secondaryElement = this.dismissibleButton();
+        }
+
+        let badge = newElement('span', { classes: `${this.styling.badge} ${themes[this.theme].badge}` })
+        let postLink = newElement('a', { classes: `${this.styling.postTitle} ${themes[this.theme].postTitle}` })
+
+        badge.innerText = this.badge;
+        postLink.href = this.postLink;
+        postLink.innerText = this.postTitle;
+
+        postLink.innerHTML += `
+            <svg class="hb-h-3 hb-w-3" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
+            </svg>
+            `
+
+        let postElement = newElement('div', {
+            classes: `${this.styling.linkWrapper} ${themes[this.theme].linkWrapper}`,
+            children: [badge, postLink]
+        })
+
+        let _hbar = newElement('div', {
+            classes: `${this.styling.wrapper} ${themes[this.theme].wrapper}`,
+            children: [postElement, secondaryElement]
+        })
+
+        let container = document.getElementById(this.ele);
+
+        container.innerHTML = ""
+        container.appendChild(_hbar)
+
+        // ? what to send out
+        this.onCompleted({ element: container, id: this.ele });
+    })
+}
+
+/**
+ * Removes the element in the case of it having issues.
+ * Rather an aggressive option.
+ *
+ * Also used when dismissing.
+ */
+export function destroy() {
+    try {
+        document.getElementById(this.ele).remove()
+
+        return true;
+    } catch (error) {
+        console.error('Unable to destroy the h-bar wrapper')
+        console.error(error)
+    }
+
+    return false;
+}
+
+/**
+ * Creates the HTML node for a dismissible button.
+ *
+ * @returns HTMLElement
+ */
+export function dismissibleButton() {
+    let dismiss = newElement('button', {
+        classes: 'hb--mr-2 hb-flex hb-p-1 hb-rounded-md hover:hb-text-white hover:hb-bg-gray-800 focus:hb-outline-none focus:hb-bg-gray-800',
+    });
+
+    dismiss.innerHTML = `<svg class="hb-h-4 hb-w-4 ${themes[this.theme].dismiss}" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>`
+
+    dismiss.onclick = (e) => {
+        e.preventDefault();
+
+        // just do it early if we not logging time.
+        if (!this.dismissFor) return this.destroy();
+
+        if (localStorage) {
+            localStorage.setItem('h-bar_dismiss_for', this.dismissFor.getTime());
+        }
+
+        return this.destroy();
+    }
+
+    return dismiss;
+}
+
+/**
+ * Determines if the banner has been dismissed.
+ *
+ * @returns boolean
+ */
+export function isDismissed() {
+
+    if (localStorage) {
+        var dismissDate = localStorage.getItem('h-bar_dismiss_for');
+        if (!dismissDate) {
+            return false;
+        }
+
+        dismissDate = dismissDate;
+        var ourDate = (new Date()).getTime();
+
+        if (ourDate <= dismissDate) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Creates the secondary links for the bar.
+ */
+export function createSecondaryLinks() {
+    if (!this.secondaryLinks) return [];
+
+    return this.secondaryLinks.map(({ title, link }) => {
+        let style = `${this.styling.secondaryLink} ${themes[this.theme].secondaryLink}`;
+        let butter = newElement('a', { classes: style })
+        butter.href = link;
+        butter.innerText = title;
+
+        return butter;
+    }, this);
+}
---
+++ src/hBar.js
@@ -0,0 +1,53 @@
+import { config } from "./config/config";
+import { styling } from "./config/styling";
+import { initNormalise } from "./functions/normalise";
+
+export default class hBar {
+
+    version = '2.0.0';
+
+    /**
+     *
+     * @param {object} options
+     * @param {string} options.url
+     * @param {string} options.ele The element id
+     * @param {boolean} options.dismissible
+     * @param {Date|boolean} options.dismissFor
+     * @param {string} options.badge
+     * @param {array} options.secondaryLinks
+     * @param {object} options.headers
+     * @param {object} options.customStyles
+     * @param {function} options.parser
+     * @param {function} options.onCompleted
+     * @param {string} options.link Manual override
+     * @param {string} options.title Manual Override
+     */
+    constructor(options) {
+        this.url = options.url;
+
+        this.ele = options.ele || 'h-bar';
+
+        // we will default to false for this
+        this.dismissible = options.dismissible || false;
+
+        this.dismissFor = options.dismissFor || false;
+
+        this.config = config;
+        this.config.fetchOptions.headers = Object.assign(config.fetchOptions.headers, options.headers)
+        this.styling = Object.assign(styling, options.customStyles);
+
+        this.secondaryLinks = options.secondaryLinks
+
+        this.onCompleted = options.onCompleted || function () { };
+
+        this.badge = options.badge || 'New';
+        this.postLink = options.link
+        this.postTitle = options.title
+
+        this.theme = options.theme
+
+        initNormalise(options.parser)
+
+        return this
+    }
+}
